3e0b7949d20c3ddc89052cf31b6aebc2f0c18f41
esahin90 <emre.tr@outlook.com>
2017-06-24 15:48:33 +0200

    gui und so krams

---
+++ lieferant-gui.py
@@ -0,0 +1,7 @@
+%matplotlib qt
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+img = mpimg.imread("pics/Long.png")
+plt.imshow(img)
---
+++ playsound.py
@@ -0,0 +1,11 @@
+import pygame
+
+import time
+
+pygame.init()
+
+pygame.mixer.music.load("resources/output.wav")
+
+pygame.mixer.music.play()
+
+time.sleep(10)
